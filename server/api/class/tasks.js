2bc584a2cc106698c2359c905c6add7bf919aa11
mferrara63 <mtferrara@umass.edu>
2021-11-03 11:08:19 -0400

    more edits to js files

--- server/api/class/tasks.js
+++ server/api/class/tasks.js
@@ -4,21 +4,49 @@
 // -Update for updating a specific task
 // -Delete for removing a specific task
 
-//ADD TASK CLICKED
-//popup add task modal
-//create task
-//add to calendar
-
-//EDIT BUTTON NEXT TO CLASS CLICKED
-//pop up edit task modal
-//read task data into edit task modal
-//update task data when update is clicked
-
-//if delete is clicked
-//delete task from tasks on side
-
-//if day on calendar is selected
-//list events on side under events for selected day
 
 //somehow have to sort events by recency and add first 4/5 to future tasks
+//Maybe do this onload?
+
+//ADD TASK CLICKED
+
+document.getElementById("addclass").addEventListener('click', () => {
+
+    //popup add task modal
+    //create task
+    //add to calendar
+
+})
+
+//EDIT BUTTON NEXT TO CLASS CLICKED
+
+document.getElementById("editclass").addEventListener('click', () => {
+
+    //pop up edit task modal
+    //read task data into edit task modal
+    //update task data when update is clicked
+
+
+})
+
+//DELETE CLASS CLICKED
+
+document.getElementById("deleteclass").addEventListener('click', () => {
+
+    //delete task from tasks on side
+
+})
+
+//DAY ON CALENDER CLICKED
+
+document.getElementById("day").addEventListener('click', () => {
+
+    //read events from selected day
+    //update list events on side under events for selected day
+
+})
+
+
+
+
 
