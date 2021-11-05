73873e370753ab2741d60552af54cac6b577b519
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2021-11-05 13:03:21 -0400

    Adjusted class api.

--- server/api/class.js
+++ server/api/class.js
@@ -6,20 +6,20 @@ const fs = require('fs');
  * @param {Request<{}, any, any, qs.ParsedQs, Record<string, any>} request 
  * @param {Response<any, Record<string, any>, number>} response 
  */
-function get(request, response) {
+async function get(request, response) {
     const user = request.body['user'];
-    const name = request.query.name;
-    if (user !== undefined && name !== undefined) {
-        if (fs.existsSync(`./users/${user}/${name}/`)) {
-            fs.readdir(`./users/${user}/${name}/`, (err, files) => {
+    const className = request.query.class;
+    if (user !== undefined && className !== undefined) {
+        if (fs.existsSync(`./users/${user}/${className}/`)) {
+            fs.readdir(`./users/${user}/${className}/`, (err, files) => {
                 if (err) {
-                    response.end(JSON.stringify({ result: `Unable to find files for class ${name}` }));
+                    response.end(JSON.stringify({ result: `Unable to find files for class ${className}` }));
                 } else {
                     response.end(JSON.stringify({ fileNames: files }));
                 }
             });    
         } else {
-            response.end(JSON.stringify({ result: `Class ${name} not found` }));    
+            response.end(JSON.stringify({ result: `Class ${className} not found` }));    
         }
     } else {
         response.end(JSON.stringify({ result: 'class not found' }));
@@ -63,23 +63,23 @@ function post(request, response) {
  * @param {Response<any, Record<string, any>, number>} response 
  */
 async function addClass(user, request, response) {
-    const name = request.body['name'];
+    const className = request.body['class'];
     const description = request.body['description'];
-    if (name !== undefined && description !== undefined) {
+    if (className !== undefined && description !== undefined) {
         // If the class folder and description already exists, respond with an error
         // Otherwise, make the folder, description, and respond with success
-        if (fs.existsSync(`./users/${user}/${name}/`)) {
-            response.end(JSON.stringify({ result: `Class ${name} already exists` }));
+        if (fs.existsSync(`./users/${user}/${className}/`)) {
+            response.end(JSON.stringify({ result: `Class ${className} already exists` }));
         } else {
-            fs.mkdir(`./users/${user}/${name}/`, (err) => {
+            fs.mkdir(`./users/${user}/${className}/`, (err) => {
                 if (err) { // Error when unable to make the directory. 
-                    response.end(JSON.stringify({ result: `Failed to add class ${name}` }));
+                    response.end(JSON.stringify({ result: `Failed to add class ${className}` }));
                 } else { // Write description if successful
-                    fs.writeFile(`./users/${user}/${name}/.description`, description, (err) => {
+                    fs.writeFile(`./users/${user}/${className}/.description`, description, (err) => {
                         if (err) { // Error when unable to write the description.
-                            response.end(JSON.stringify({ result: `Added class ${name}, but failed to add description` }));
+                            response.end(JSON.stringify({ result: `Added class ${className}, but failed to add description` }));
                         } else { // Success on writing the description
-                            response.end(JSON.stringify({ result: `Added class ${name} and description` }));
+                            response.end(JSON.stringify({ result: `Added class ${className} and description` }));
                         }
                     });
                 }
@@ -95,22 +95,22 @@ async function addClass(user, request, response) {
  * @param {Request<{}, any, any, qs.ParsedQs, Record<string, any>} request 
  * @param {Response<any, Record<string, any>, number>} response 
  */
-function editClass(user, request, response) {
-    const name = request.body['name'];
+ async function editClass(user, request, response) {
+    const className = request.body['class'];
     const description = request.body['description'];
-    if (name !== undefined && description !== undefined) {
+    if (className !== undefined && description !== undefined) {
         // If the class folder and description exist, replace the description
         // Otherwise, respond with an error
-        if (fs.existsSync(`./users/${user}/${name}/`)) {
-            fs.writeFile(`./users/${user}/${name}/.description`, description, (err) => {
+        if (fs.existsSync(`./users/${user}/${className}/`)) {
+            fs.writeFile(`./users/${user}/${className}/.description`, description, (err) => {
                 if (err) { // Error when unable to write the description.
-                    response.end(JSON.stringify({ result: `Failed to add description for class ${name}` }));
+                    response.end(JSON.stringify({ result: `Failed to add description for class ${className}` }));
                 } else { // Success on writing the description
-                    response.end(JSON.stringify({ result: `Edited description for class ${name}` }));
+                    response.end(JSON.stringify({ result: `Edited description for class ${className}` }));
                 }
             });
         } else {
-            response.end(JSON.stringify({ result: `Class ${name} doesn't exist` }));
+            response.end(JSON.stringify({ result: `Class ${className} doesn't exist` }));
         }
     } else {
         response.end(JSON.stringify({ result: 'edit class failed' }));
@@ -123,16 +123,16 @@ function editClass(user, request, response) {
  * @param {Response<any, Record<string, any>, number>} response 
  */
  async function removeClass(user, request, response) {
-    const name = request.body['name'];
-    if (name !== undefined) {
+    const className = request.body['class'];
+    if (className !== undefined) {
 
         // If the class folder exists, delete it
         // Otherwise, respond with an error
-        if (fs.existsSync(`./users/${user}/${name}/`)) {
-            fs.rmSync(`./users/${user}/${name}/`, { recursive: true, force: true });
-            response.end(JSON.stringify({ result: `Removed class ${name}` }));
+        if (fs.existsSync(`./users/${user}/${className}/`)) {
+            fs.rmSync(`./users/${user}/${className}/`, { recursive: true, force: true });
+            response.end(JSON.stringify({ result: `Removed class ${className}` }));
         } else {
-            response.end(JSON.stringify({ result: `Class ${name} doesn't exist` }));
+            response.end(JSON.stringify({ result: `Class ${className} doesn't exist` }));
         }
         
     } else {
