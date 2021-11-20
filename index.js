362f4fed0bf6e24700acd1fde6725cb9d1f5d969
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2021-11-19 20:23:50 -0500

    Commented index.js

--- index.js
+++ index.js
@@ -16,19 +16,23 @@ app.use(express.static('./public')); // serve public files
 app.use(express.static('./public/css')); // serve public files
 app.use(express.static('./public/js')); // serve public files
 
+// Set the signup and login endpoints
 app.post("/api/signup", userAPI.postSignup);
 app.post("/api/login", userAPI.postLogin);
 
+// Set the user endpoints
 app.get("/api/users/:user", userAPI.getData);
 app.post("/api/users/:user/edit", userAPI.postEdit);
 app.post("/api/users/:user/delete", userAPI.postDelete);
 
+// Set the task endpoints
 app.get("/api/users/:user/tasks", taskAPI.getAll);
 app.post("/api/users/:user/tasks/create", taskAPI.postCreate);
 app.post("/api/users/:user/tasks/:taskid", taskAPI.getTask);
 app.post("/api/users/:user/tasks/:taskid/edit", taskAPI.postEdit);
 app.post("/api/users/:user/tasks/:taskid/remove", taskAPI.postRemove);
 
+// Set the class endpoints
 app.get("/api/users/:user/class/", classAPI.getAll);
 app.get("/api/users/:user/class/:class", classAPI.getClass);
 app.post("/api/users/:user/class/:class/create", classAPI.postCreate);
@@ -36,10 +40,12 @@ app.post("/api/users/:user/class/:class/edit", classAPI.postEdit);
 app.post("/api/users/:user/class/:class/remove", classAPI.postRemove);
 app.get("/api/users/:user/class/:class/search", classAPI.getSearch);
 
+// Set the tag endpoints
 app.get("/api/users/:user/tags", tagAPI.getAll);
 app.post("/api/users/:user/tags/:tag/create", tagAPI.postCreate);
 app.post("/api/users/:user/tags/:tag/remove", tagAPI.postRemove);
 
+// Set the note endpoints
 app.get("/api/users/:user/class/:class/notes/:note", noteAPI.getNote);
 app.post("/api/users/:user/class/:class/notes/:note/create", noteAPI.postCreate);
 app.get("/api/users/:user/class/:class/notes/:note/tags", noteAPI.getTags);
@@ -48,10 +54,9 @@ app.post("/api/users/:user/class/:class/notes/:note/addTags", noteAPI.postAddTag
 app.post("/api/users/:user/class/:class/notes/:note/remove", noteAPI.postRemove);
 app.post("/api/users/:user/class/:class/notes/:note/removeTags", noteAPI.postRemoveTags);
 
-
+// Set the flashcard endpoints
 app.get("/api/users/:user/class/:class/flashcards/:flashcard", flashcardAPI.getFlashcards);
 app.get("/api/users/:user/class/:class/flashcards/:flashcard/tags", flashcardAPI.getTags);
-
 app.post("/api/users/:user/class/:class/flashcards/:flashcard/create", flashcardAPI.postCreate);
 app.post("/api/users/:user/class/:class/flashcards/:flashcard/remove", flashcardAPI.postRemove);
 app.post("/api/users/:user/class/:class/flashcards/:flashcard/addTags", flashcardAPI.postAddTags);
@@ -59,8 +64,10 @@ app.post("/api/users/:user/class/:class/flashcards/:flashcard/removeTags", flash
 app.post("/api/users/:user/class/:class/flashcards/:flashcard/addFlashcard", flashcardAPI.postAddFlashcard);
 app.post("/api/users/:user/class/:class/flashcards/:flashcard/removeFlashcard", flashcardAPI.postRemoveFlashcard);
 
+// Set the default handler
 app.get("*", defaultHandler);
 
+// Get the secrets
 let secrets;
 let uri;
 if (!process.env.URI) {
@@ -69,8 +76,12 @@ if (!process.env.URI) {
 } else {
     uri = process.env.URI;
 }
+
+// Connect to the database
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => { });
+
+// Start the server
 app.listen(process.env.PORT || 8080, () => console.log(`Server listening on http://localhost:${process.env.PORT || 8080}`));
 
 /**
@@ -80,3 +91,5 @@ app.listen(process.env.PORT || 8080, () => console.log(`Server listening on http
 function defaultHandler(request, response) {
     response.end(JSON.stringify({ result: 'error', path: __dirname, request: request.path }));
 }
+
+module.exports = { client }
