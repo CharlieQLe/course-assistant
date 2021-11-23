e0b03d6cabcfbad2692730586c03133b399c76c8
Jerry Yu <jerryyu@1x-nat-vl932-172-30-211-152.wireless.umass.edu>
2021-11-23 01:30:08 -0500

    flashcards and notes db should work with front-end but still needs work on error handling

--- public/js/notepad.js
+++ public/js/notepad.js
@@ -1,25 +1,44 @@
 
 window.addEventListener('load', async function() {
-
+	const url = window.location.pathname;       // reads url
+	const split = url.split('/');
 	// post to server. tell server to save file
-	fetch('/api/users/USER/class/CLASS/notes/NOTE/edit', {
-		method: 'POST', 
-		body: JSON.stringify({ body: document.getElementById('notepad-textarea').value }), 
-		headers: {
-			'Content-Type': 'application/json',
-		}
-	});
+	fetch(`/api/users/${split[2]}/class/${split[4]}/notes/${split[6]}`)
+	.then(response => {
+        return response.json();
+    }).then(obj => {
+        // if we get a status code of 200, set the client-side flashcard set 
+        // with flashcard set from server
+        console.log(obj)
+        if (obj.status === 200) {
+            document.getElementById('notepad-textarea').value = obj.result;
+        } else {
+            throw 'something went wrong with getting the notes from the server: ' + obj.result;
+        }
+
+    }).catch(e => {
+        console.log(e);
+    });
+	
+	// fetch('/api/users/USER/class/CLASS/notes/NOTE/edit', {
+	// 	method: 'POST', 
+	// 	body: JSON.stringify({ body: document.getElementById('notepad-textarea').value }), 
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 	}
+	// });
 
 	// sets the file name of the notes
-	const url = window.location.pathname;		// reads the url 
-	const split = url.split('/');
 	document.getElementById('notepad-title').innerHTML = split[split.length-1];
 })
 
 document.getElementById('save-btn').addEventListener('click', () => {
-	
+	const url = window.location.pathname;       // reads url
+	const split = url.split('/');
+	console.log(split);
+	console.log(document.getElementById('notepad-textarea').value)
 	// post to server. tell server to save file
-	fetch('/api/users/USER/class/CLASS/notes/NOTE/edit', {
+	fetch(`/api/users/${split[2]}/class/${split[4]}/notes/${split[6]}/edit`, {
 		method: 'POST', 
 		body: JSON.stringify({ body: document.getElementById('notepad-textarea').value }), 
 		headers: {
