b075a64bd7d83756cb0875f9fa2d76d2f971f8dd
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2021-11-30 12:15:07 -0500

    Finished tag api.

--- api/tags.js
+++ api/tags.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { client } = require('./mongo.js');
+
 /**
  * Process a get request to retrieve all the tags.
  * 
@@ -7,7 +9,24 @@
  * @param {Response<any, Record<string, any>, number>} response 
  */
 function getAll(request, response) {
-    response.end(JSON.stringify({ result: "Get all tags received!" }));
+    client.db("final-kappa").collection(request.query.user).find({ type: "tag" }).toArray((err, tags) => {
+        if (err) {
+            response.end(JSON.stringify({
+                statusCode: -1,
+                data: "Failed to search for every tag"
+            }));
+        } else {
+            response.end(JSON.stringify({
+                statusCode: 0,
+                data: tags.map(x => {
+                    return {
+                        name: x.name,
+                        type: x.type
+                    };
+                })
+            }));
+        }
+    })
 }
 
 /**
@@ -17,7 +36,34 @@ function getAll(request, response) {
  * @param {Response<any, Record<string, any>, number>} response 
  */
 function postCreate(request, response) {
-    response.end(JSON.stringify({ result: "Create tag received!" }));
+    client.db("final-kappa").collection(request.params.user).findOne({
+        name: request.params.tag,
+        type: "tag"
+    }, (err, existingTag) => {
+        if (err || existingTag) {
+            response.end(JSON.stringify({
+                statusCode: -1,
+                data: "Failed to add tag"
+            }));
+        } else {
+            client.db("final-kappa").collection(request.params.user).insertOne({
+                name: request.params.tag,
+                type: "tag"
+            }, (err, res) => {
+                if (err) {
+                    response.end(JSON.stringify({
+                        statusCode: -1,
+                        data: "Failed to add tag"
+                    }));
+                } else {
+                    response.end(JSON.stringify({
+                        statusCode: 0,
+                        data: "Added tag"
+                    }));
+                }
+            });
+        }
+    });
 }
 
 /**
@@ -27,7 +73,22 @@ function postCreate(request, response) {
  * @param {Response<any, Record<string, any>, number>} response 
  */
 function postRemove(request, response) {
-    response.end(JSON.stringify({ result: "Remove tag received!" }));
+    client.db("final-kappa").collection(request.params.user).deleteOne({
+        name: request.params.tag,
+        type: "tag"
+    }, (err, res) => {
+        if (err) {
+            response.end(JSON.stringify({
+                statusCode: -1,
+                data: "Failed to delete tag"
+            }));
+        } else {
+            response.end(JSON.stringify({
+                statusCode: 0,
+                data: "Deleted tag"
+            }));
+        }
+    });
 }
 
 module.exports = { getAll, postCreate, postRemove };
