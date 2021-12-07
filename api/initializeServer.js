740f32ce785371a0b6ad0528bd229f40e8755913
Charlie Le <20309750+CharlieQLe@users.noreply.github.com>
2021-12-07 13:24:44 -0500

    Adjusted secrets reading.

--- api/initializeServer.js
+++ api/initializeServer.js
@@ -9,22 +9,14 @@ const mc = new minicrypt();
 if (fs.existsSync('./secrets.json')) {
     const secrets = JSON.parse(fs.readFileSync('./secrets.json'));
     if (!process.env.SECRET) {
-        process.env.SECRET = secrets.session;
+        process.env.SECRET = secrets.uri;
     }
-    if (!process.env.URI) {
-        process.env.URI = secrets.uri;
-    }
-} else {
-    if (!process.env.URI) {
-        console.log('no URI for database!');
-    }
-    if (!process.env.SECRET) {
-        console.log('no secrets!');
-    }
+} else if (!process.env.SECRET) {
+    console.log('no secrets!');
 }
 
 // Connect to the database
-const client = new MongoClient(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(process.env.SECRET, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(_ => { });
 
 module.exports = { client, mc };
